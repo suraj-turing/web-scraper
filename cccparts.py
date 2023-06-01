c1cf3fede40f95c034b17a8e575a651cdb25cec7
suraj-turing <105240754+suraj-turing@users.noreply.github.com>
2023-06-01 11:25:47 +0530

    Update cccparts.py

--- cccparts.py
+++ cccparts.py
@@ -10,6 +10,7 @@ from dotenv import dotenv_values
 import sys
 
 def configure_driver():
+    print("configuring driver...")
     # Configure Selenium options
     options = Options()
     options.add_argument("--headless")  # Run Chrome in headless mode
@@ -20,6 +21,7 @@ def configure_driver():
 
 def scrape_web_page(url):
     driver = configure_driver()
+    print("scraping web page...", url)
     # Load the page with Selenium
     driver.get(url)
     # Wait for the page to fully load
@@ -29,21 +31,30 @@ def scrape_web_page(url):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     # Close the Selenium browser
     driver.quit()
+    print("scraping complete...")
     return soup
 
 def extract_product_details(soup):
+    print("extracting product details...")
     product_details = {} # initialize a dictionary to store product details
 
+    # Extract All Specifications
+    global specifications
+    specifications = {}
+    specifications_list = soup.find_all('div', class_='pdp-feature-spec-heading')
+    for specification in specifications_list:
+        key = specification.text.strip().split(":")[0]
+        value = specification.find_next_sibling('div').text.strip()
+        specifications[key] = value
+
     # Extract details and store them in the dictionary
-    product_details["Vendor Part Number"] = soup.find('span', class_='product-vendor-value').text.strip()
+    product_details["Vendor Part Number"] = specifications["Vendor Part Number"]
     
     # Brand
-    brand_div = soup.find('div', class_='pdp-feature-spec-heading', string='Brand:')
-    product_details["Brand"] = brand_div.find_next_sibling('div').text.strip() if brand_div else None
+    product_details["Brand"] = specifications["Brand"]
 
     # Part Name
-    product_details["Part Name"] = {}
-    product_details["Part Name"]["current"] = soup.find('div', class_='product-name').text.strip()
+    product_details["Part Name"] = soup.find('div', class_='product-name').text.strip()
 
     # Categories
     category_element = soup.find('div', class_='breadCrumb')
@@ -57,7 +68,7 @@ def extract_product_details(soup):
     else:
         print("Category element not found")
 
-    categories = soup.find('div', class_='pdp_items', string=lambda text: text and text.startswith('Parts Classification')).text.strip().split(">>")
+    categories = specifications["Parts Classification"].split(">>")
     product_details["Major Category"] = categories[1]
     product_details["Minor Category"] = categories[2]
     product_details["Sub-minor Category"] = categories[3]
@@ -66,12 +77,13 @@ def extract_product_details(soup):
     product_details["URL"] = url
 
     # Description
-    product_details["Description"] = {}
-    product_details["Description"]["current"] = soup.find('div', class_='pdp-long-description').text.strip()
+    product_details["Description"] = soup.find('div', class_='pdp-long-description').text.strip()
 
+    print("extracting product details complete...")
     return product_details
 
-def generate(data, temperature=1, max_tokens=100):
+def generate(data, temperature=1, max_tokens=1000):
+    print("generating with openai...")
     # Your OpenAI API key
     openai.api_key = dotenv_values()["OPENAI_API_KEY"]
 
@@ -87,6 +99,7 @@ def generate(data, temperature=1, max_tokens=100):
 
     generated_description = response.choices[0].text.strip()
 
+    print("generation with openai complete...")
     return generated_description
 
 def main(url):
@@ -96,14 +109,32 @@ def main(url):
     # Extract product details
     product_details = extract_product_details(soup)
 
-    # Generate description and name
-    product_details["Description"]["generated"] = generate(str(product_details))
-    product_details["Part Name"]["generated"] = generate(str(product_details), 0.7, 10)
+    # Prepare Openai Payload
+    opanai_payload = product_details
+    del opanai_payload["URL"]
+    print("OpenAI Payload: ", json.dumps(opanai_payload, indent=4))
 
+    # Generate description and name
+    generated_description = generate(str(opanai_payload))
+    print("Generated Description: ", generated_description)
+    generated_name = generate(str(opanai_payload), 0.7, 5)
+    print("Generated Name: ", generated_name)
+
+    product_details["Part Name"] = {
+        "current": product_details["Part Name"],
+        "generated": generated_name
+    }
+
+    product_details["Description"] = {
+        "current": product_details["Description"],
+        "generated": generated_description
+    }
+
+    product_details["Specifications"] = specifications
     # Convert the product details to JSON
     json_data = json.dumps(product_details, indent=4)
     # Print the JSON data
-    print(json_data)
+    print("\n\n\nOUTPUT:", json_data)
 
 # Run the script
 if __name__ == "__main__":
